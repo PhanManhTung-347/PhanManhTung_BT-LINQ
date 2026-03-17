5b19f144a0ad97f3625a7b280d9490564c742073
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-03-17 19:05:33 +0700

    PhanManhTungnopbai14

---
+++ PhanManhTung_Bai14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Bai14
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ho va ten: Phan Manh Tung || Msv: 2415053122347|| LHP: 225LTC03");
+        Console.WriteLine("Bai 14: Kiem tra sinh vien co rot khong.");
+        List<Student> students = new List<Student>() {
+            new Student { Id = 1, Name = "An", Score = 8 },
+            new Student { Id = 2, Name = "Binh", Score = 6 },
+            new Student { Id = 3, Name = "Chi", Score = 9 },
+            new Student { Id = 4, Name = "Dung", Score = 7 } };
+        Console.WriteLine("Danh sach sinh vien:");
+        foreach (Student sv in students)
+            Console.WriteLine($"ID={sv.Id,-5} || Name={sv.Name,-10} || Score={sv.Score,-5}");
+        var svrot = students.Any(sv=> sv.Score<5);
+        if (svrot)
+            Console.WriteLine("Ket Qua: Co sinh vien rot trong danh sach.");
+        else
+            Console.WriteLine("Ket Qua: Khong co sinh vien nao rot.");
+    }
+}
