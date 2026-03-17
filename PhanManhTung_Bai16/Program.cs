a011c147cf8b839fb8d3ce62961bd1da83a4105d
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-03-17 19:08:20 +0700

    PhanManhTungnopbaitap16

---
+++ PhanManhTung_Bai16/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Bai16
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ho va ten: Phan Manh Tung || Msv: 2415053122347|| LHP: 225LTC03");
+        Console.WriteLine("Bai 16: Tinh tong cac phan tu.");
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+        Console.WriteLine("Danh sach cac phan tu:");
+        foreach (var pt in numbers)
+            Console.Write(pt + " ");
+        var tong = numbers.Sum();
+        Console.WriteLine($"\nTong cac phan tu trong danh sach la:{tong}");
+
+    }
+}
