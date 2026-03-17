a92a940f8d47e71eeab1a5e226cdd65d36354fa0
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-03-17 19:13:32 +0700

    PhanManhTungnopbaitap19

---
+++ PhanManhTung_Bai19/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Bai19
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ho va ten: Phan Manh Tung || Msv: 2415053122347|| LHP: 225LTC03");
+        Console.WriteLine("Bai 19: Tim so lan xuat hien nhieu nhat.");
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 2, 5 };
+        Console.WriteLine("Danh sach cac phan tu:");
+        foreach (var pt in numbers)
+            Console.Write(pt + " ");
+        var group = numbers.GroupBy(pt => pt);
+        var ptt = group.OrderByDescending(gr => gr.Count()).FirstOrDefault();
+        if (ptt != null)
+            Console.WriteLine($"\nPhan tu {ptt.Key} xuat hien nhieu nhat voi {ptt.Count()} lan.");
+    }
+}
