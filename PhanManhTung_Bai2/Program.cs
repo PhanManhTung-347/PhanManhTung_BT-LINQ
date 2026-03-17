396100013ca208ff45be87335d58f15b0ae4b48e
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-03-17 18:38:58 +0700

    PhanManhTungnopbaitap2

---
+++ PhanManhTung_Bai2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Bai2
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ho va ten: Phan Manh Tung || Msv: 2415053122347|| LHP: 225LTC03");
+        Console.WriteLine("Bai 2: Loc so lon hon 5.");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+        Console.WriteLine("Danh sach cac phan tu:");
+        foreach (var pt in numbers)
+        {
+            Console.Write(pt + " ");
+        }
+        var ds = numbers.Where(p => p>5);
+        Console.WriteLine("\nDanh sach cac so lon hon 5 la:");
+        foreach (var sc in ds)
+        {
+            Console.Write(sc + " ");
+        }
+    }
+}
