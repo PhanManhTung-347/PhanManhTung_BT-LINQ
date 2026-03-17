cae02d990570c9629d89ae8573816bd876eeffdf
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-03-17 18:57:02 +0700

    PhanManhTungnopbaitap9

---
+++ PhanManhTung_Bai9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+
+class Bai8
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ho va ten: Phan Manh Tung || Msv: 2415053122347|| LHP: 225LTC03");
+        Console.WriteLine("Bai 9: Lay danh sach ten sinh vien");
+        List<Student> students = new List<Student>() {
+            new Student { Id = 1, Name = "An", Score = 8 },
+            new Student { Id = 2, Name = "Binh", Score = 6 },
+            new Student { Id = 3, Name = "Chi", Score = 9 },
+            new Student { Id = 4, Name = "Dung", Score = 7 } };
+        Console.WriteLine("Danh sach sinh vien:");
+        foreach (Student sv in students) 
+            Console.WriteLine($"ID={sv.Id,-5} || Name={sv.Name,-5} || Score={sv.Score,-5}");
+        var dssv = students.Select(sv => sv.Name);
+        Console.WriteLine("Danh sach ten sinh vien:");
+        foreach (var sv in dssv)
+            Console.WriteLine(sv);
+    }
+}
