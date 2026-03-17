84060d73fd1f02c7881bc825bb2183a90310703b
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-03-17 19:06:51 +0700

    PhanManhTungnopbaitap15

---
+++ PhanManhTung_Bai15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Bai15
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ho va ten: Phan Manh Tung || Msv: 2415053122347|| LHP: 225LTC03");
+        Console.WriteLine("Bai 15: Loai bo phan tu trung");
+        List<int> numbers = new List<int> { 1,2,2,3,4,4,5 };
+        Console.WriteLine("Danh sach cac phan tu:");
+        foreach (var pt in numbers)
+            Console.Write(pt+" ");
+        var dsnew = numbers.Distinct();
+        Console.WriteLine("\nDanh sach sau khi loai bo phan tu trung:");
+        foreach (var pt in dsnew)
+            Console.Write(pt+" ");
+        
+    }
+}
